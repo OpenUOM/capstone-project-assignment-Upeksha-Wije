4c16d067a1723b1a5bec90ece9d8b62917c054f2
Upeksha <upekshawijeratne8@gmail.com>
2022-09-06 20:57:19 +0530

    11

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -60,7 +60,7 @@ export class TeacherTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search(value="Kusuma Ranasinghe") {
     let foundItems = [];
     if (value.length <= 0) {
       this.getTeacherData();
